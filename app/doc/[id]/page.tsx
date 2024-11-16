3b9e1e9aa71a50ac5d5fc0b23695ea5a7a88ac21
Mayank <mayanksingh8181335335@gmail.com>
2024-11-16 15:03:57 +0530

    updates

--- app/doc/[id]/page.tsx
+++ app/doc/[id]/page.tsx
@@ -3,7 +3,7 @@
 import Document from '@/components/Document';
 import React from 'react'
 
-function DocumentPage({params:{id}}:{params:{params:{id:string}}}) {
+function DocumentPage({params:{id}}:{params:{id:string;}}) {
   return (
     <div className="flex flex-col flex-1 min-h-screen">
         <Document id={id}/>

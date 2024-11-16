7a5c917d277313915ce167b5935a8812fdff53fd
Mayank <mayanksingh8181335335@gmail.com>
2024-11-16 21:53:09 +0530

    partially added owner option

--- components/Document.tsx
+++ components/Document.tsx
@@ -44,7 +44,7 @@ function Document({id}:{id:string}) {
 
 
   return (
-    <div>
+    <div className="flex-1 h-full bg-white p-5">
 
     <div className='flex max-w-6xl mx-auto justify-between p-5'>
       <form 

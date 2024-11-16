8aa83ec00c48460e7a386765b50d08327e1647c3
Mayank <mayanksingh8181335335@gmail.com>
2024-11-17 02:00:21 +0530

    added delete document feature

--- components/DeleteDocument.tsx
+++ components/DeleteDocument.tsx
@@ -13,6 +13,9 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "./ui/button";
 import { DialogClose } from "@radix-ui/react-dialog";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
+import { deleteDocument } from "@/actions/actions";
 
 function DeleteDocument() {
 
@@ -20,15 +23,35 @@ function DeleteDocument() {
 
     const [isPending,startTransition]=useTransition();
 
-    const handleDelete=async ()=>{
+    const pathname=usePathname();
 
+    const router=useRouter();
+
+    const handleDelete=async ()=>{
+        const roomId=pathname.split("/").pop();
+        if(!roomId)
+        {
+            return;
+        }
+        startTransition(async ()=>{
+            const {success}= await deleteDocument(roomId);
+
+            if(success){
+                setIsOpen(false);
+                router.replace("/");
+                // toast.success("Room Deleted successfully!");
+            }
+            else{
+                // toast.error("Failed to delete room!");
+            }
+        })
     }
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
         <Button asChild variant="destructive">
 
-      <DialogTrigger>Open</DialogTrigger>
+      <DialogTrigger>Delete</DialogTrigger>
         </Button>
       <DialogContent>
         <DialogHeader>
@@ -41,6 +64,7 @@ function DeleteDocument() {
     <DialogFooter className="sm:justify-end gap-2">
 
         <Button
+        className="mx-auto"
         type="button"
         variant="destructive"
         onClick={handleDelete}
